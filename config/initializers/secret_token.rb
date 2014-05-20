722dd2dc158b270fe4c0911f341d8bb4a939e033
sakshyamdahal <dahalsakshyam@gmail.com>
2014-05-20 05:55:07 -0700

    start cms application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CmsApplication::Application.config.secret_key_base = '5ba903ecb9d564ccfcce5761b593fb21045387b96dc2d8804300413c8e3567b1212045365905c221c57784b84d7bedf14ba4e89c1c99adafc8085320493a4869'
